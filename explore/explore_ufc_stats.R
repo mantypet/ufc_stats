36b0e9038a42973719ded1520412e3a759013ab3
Petteri Mäntymaa <petteri.mantymaa@gmail.com>
2022-11-06 21:19:01 +0200

    reporting data

--- explore/explore_ufc_stats.R
+++ explore/explore_ufc_stats.R
@@ -1,3 +1,11 @@
+source(here::here("R/readers.R"))
 
-ufc_stats %>%
-  group_by()
+load_ufc_stats()
+
+ufc_stats.rep <- ufc_stats %>%
+  filter(scheduled_rounds %in% c("3 Rnd (5-5-5)", "5 Rnd (5-5-5-5-5)")) %>%
+  filter(winner %in% c("W", "L"))
+
+ufc_stats.rep.wide <- ufc_stats.rep %>%
+  pivot_wider(id_cols = c(winner, fighter), names_from = winner, values_from = fighter)
+
